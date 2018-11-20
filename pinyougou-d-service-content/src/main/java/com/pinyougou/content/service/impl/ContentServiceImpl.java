d39fea6f768e7770643147b87242552e864886e3
MuziLee-noob <41133505+MuziLee-noob@users.noreply.github.com>
2018-11-20 14:11:08 +0800

    开发功能：网站首页-广告首页-展示轮播图

--- pinyougou-d-service-content/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou-d-service-content/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -1,19 +1,23 @@
 package com.pinyougou.content.service.impl;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.pinyougou.content.service.ContentService;
 import com.pinyougou.mapper.TbContentMapper;
 import com.pinyougou.pojo.TbContent;
 import com.pinyougou.pojo.TbContentExample;
 import com.pinyougou.pojo.TbContentExample.Criteria;
-import com.pinyougou.content.service.ContentService;
 
 import entity.PageResult;
 
 /**
  * 服务实现层
+ * 
  * @author Administrator
  *
  */
@@ -22,7 +26,7 @@ public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper contentMapper;
-	
+
 	/**
 	 * 查询全部
 	 */
@@ -36,8 +40,8 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public PageResult findPage(int pageNum, int pageSize) {
-		PageHelper.startPage(pageNum, pageSize);		
-		Page<TbContent> page=   (Page<TbContent>) contentMapper.selectByExample(null);
+		PageHelper.startPage(pageNum, pageSize);
+		Page<TbContent> page = (Page<TbContent>) contentMapper.selectByExample(null);
 		return new PageResult(page.getTotal(), page.getResult());
 	}
 
@@ -46,25 +50,25 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public void add(TbContent content) {
-		contentMapper.insert(content);		
+		contentMapper.insert(content);
 	}
 
-	
 	/**
 	 * 修改
 	 */
 	@Override
-	public void update(TbContent content){
+	public void update(TbContent content) {
 		contentMapper.updateByPrimaryKey(content);
-	}	
-	
+	}
+
 	/**
 	 * 根据ID获取实体
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@Override
-	public TbContent findOne(Long id){
+	public TbContent findOne(Long id) {
 		return contentMapper.selectByPrimaryKey(id);
 	}
 
@@ -73,37 +77,47 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public void delete(Long[] ids) {
-		for(Long id:ids){
+		for (Long id : ids) {
 			contentMapper.deleteByPrimaryKey(id);
-		}		
+		}
 	}
-	
-	
-		@Override
+
+	@Override
 	public PageResult findPage(TbContent content, int pageNum, int pageSize) {
 		PageHelper.startPage(pageNum, pageSize);
-		
-		TbContentExample example=new TbContentExample();
+
+		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
-		
-		if(content!=null){			
-						if(content.getTitle()!=null && content.getTitle().length()>0){
-				criteria.andTitleLike("%"+content.getTitle()+"%");
+
+		if (content != null) {
+			if (content.getTitle() != null && content.getTitle().length() > 0) {
+				criteria.andTitleLike("%" + content.getTitle() + "%");
 			}
-			if(content.getUrl()!=null && content.getUrl().length()>0){
-				criteria.andUrlLike("%"+content.getUrl()+"%");
+			if (content.getUrl() != null && content.getUrl().length() > 0) {
+				criteria.andUrlLike("%" + content.getUrl() + "%");
 			}
-			if(content.getPic()!=null && content.getPic().length()>0){
-				criteria.andPicLike("%"+content.getPic()+"%");
+			if (content.getPic() != null && content.getPic().length() > 0) {
+				criteria.andPicLike("%" + content.getPic() + "%");
 			}
-			if(content.getStatus()!=null && content.getStatus().length()>0){
-				criteria.andStatusLike("%"+content.getStatus()+"%");
+			if (content.getStatus() != null && content.getStatus().length() > 0) {
+				criteria.andStatusLike("%" + content.getStatus() + "%");
 			}
-	
+
 		}
-		
-		Page<TbContent> page= (Page<TbContent>)contentMapper.selectByExample(example);		
+
+		Page<TbContent> page = (Page<TbContent>) contentMapper.selectByExample(example);
 		return new PageResult(page.getTotal(), page.getResult());
 	}
-	
+
+	@Override
+	public List<TbContent> findByCategoryId(Long id) {
+		TbContentExample example = new TbContentExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andCategoryIdEqualTo(id);// 指定条件分类id
+		criteria.andStatusEqualTo("1");// 指定条件为有效
+		example.setOrderByClause("sort_order");// 排序
+		List<TbContent> list = contentMapper.selectByExample(example);
+		return list;
+	}
+
 }
