b99cec0cd671eb5101af366a04cc93a1cb38b24c
MuziLee-noob <41133505+MuziLee-noob@users.noreply.github.com>
2018-11-22 21:39:58 +0800

    功能优化：搜索结果高亮显示

--- pinyougou-e-web-search/src/main/webapp/js/base.js
+++ pinyougou-e-web-search/src/main/webapp/js/base.js
@@ -1,1 +1,8 @@
-	var app = angular.module('pinyougou', []);
+var app = angular.module('pinyougou', []);
+	// 定义过滤器
+	app.filter('trustHtml', ['$sce', function($sce) {
+		//data表示被过滤的内容
+		return function(data) {
+			return $sce.trustAsHtml(data);//返回过滤后的内容，信任data中的html内容
+		}
+	}]);
