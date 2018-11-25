7e2c0d6265574e9c9ca8170c7d92473f0ed9b4e2
MuziLee-noob <41133505+MuziLee-noob@users.noreply.github.com>
2018-11-25 17:12:29 +0800

    新增功能：搜索页面优化，主页关联搜索

--- pinyougou-e-web-portal/src/main/webapp/js/controller/contentController.js
+++ pinyougou-e-web-portal/src/main/webapp/js/controller/contentController.js
@@ -9,4 +9,17 @@ app.controller('contentController', function($scope, contentService){
             }
         );
     }
+
+    //搜索（传递参数）
+    $scope.search = function() {
+        location.href = "http://localhost:9104/search.html#?keywords=" + $scope.keywords;
+    }
+
+    //让搜索框响应回车键
+	$scope.keyup = function(e) { 
+		var keycode = window.event?e.keyCode:e.which;
+		if (keycode === 13) {
+			$scope.search();
+		}
+	}
 });
